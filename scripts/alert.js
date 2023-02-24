b578656952de11db759d662ccd880ae79941a2fb
Jacob Kerr <jck268@cornell.edu>
2023-02-23 19:09:38 -0500

    Added scraper and badge systems'

---
+++ scripts/alert.js
@@ -0,0 +1,6 @@
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    let numPosts = changes.numPosts.newValue;
+    if (numPosts === 0) (async () => await chrome.action.setBadgeText({text: null}))();
+    else (async () => await chrome.action.setBadgeText({text: numPosts.toString()}))();
+    chrome.action.setBadgeBackgroundColor({color: '#9688F1'}); 
+  });
---
+++ scripts/scraper.js
@@ -0,0 +1,36 @@
+//@ts-check
+
+/**
+ * Sleep for the given duration
+ * @param {number} ms - duration in milliseconds
+ * @returns {Promise} promise to wait on
+ */
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+
+/**
+ * Gets the ids of the newest posts after the given id
+ * @returns {number} the list of ids of posts seen after the post of the given id
+ */
+function getNumNewestPosts() {
+    const posts = document.querySelectorAll('span.dft-unseen-icon');
+    return posts.length;
+}
+
+
+/**
+ * The main function
+ */
+async function main() {
+    while(true) {
+        let numPosts = getNumNewestPosts();
+        console.log(numPosts + " new " + ((numPosts == 1) ? "post" : "posts"));
+        //@ts-expect-error
+        await chrome.storage.local.set({ numPosts: numPosts })
+        await sleep(5000);
+    }
+}
+
+main()
