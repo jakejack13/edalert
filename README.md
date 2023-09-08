ed2453a57d322d9edb88db996478f6f25d698c40
Mitchell Gray <67762738+MitchellGray100@users.noreply.github.com>
2023-09-08 14:52:29 -0400

    MG1: fixed error, added popup, added readme (#1)
    * Fixed error, Added Popup, Added README
    
    * Fixed ASCII art
    
    * Fixed ASCII art and github link

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # edalert
+
+## Getting Started
+- Clone the repository.
+- Open Chrome.
+- Click the three dots in the top right.
+- Go to Extensions -> Manage Extensions.
+- Enable Developer Mode.
+- Load unpacked.
+- Select the entire repo folder.
+- Reload Chrome completely.
--- scripts/scraper.js
+++ scripts/scraper.js
@@ -1,42 +1,28 @@
-//@ts-check
-
-/**
- * Sleep for the given duration
- * @param {number} ms - duration in milliseconds
- * @returns {Promise} promise to wait on
- */
-function sleep(ms) {
-    return new Promise(r => setTimeout(r, ms));
-}
-
-
-/**
- * Gets the ids of the newest posts after the given id
- * @returns {number} the list of ids of posts seen after the post of the given id
- */
-function getNumNewestPosts() {
-    const posts = document.querySelectorAll('span.dft-unseen-icon');
-    return posts.length;
-}
-
-
-/**
- * The main function
- */
 async function main() {
-    while(true) {
-        let numPosts = getNumNewestPosts();
-        console.log(numPosts + " new " + ((numPosts == 1) ? "post" : "posts"));
-        //@ts-expect-error
-        chrome.storage.local.get(["key"]).then(async (/** @type {number} */ result) => {
-            if (result !== numPosts) {
-                //@ts-expect-error
-                await chrome.storage.local.set({ numPosts: numPosts });                
+    let shouldContinue = true;
+
+    window.addEventListener('beforeunload', () => {
+        shouldContinue = false;
+    });
+
+    while(shouldContinue) {
+        try {
+            let numPosts = getNumNewestPosts();
+            console.log(numPosts + " new " + ((numPosts == 1) ? "post" : "posts"));
+            //@ts-expect-error
+            await chrome.storage.local.get(["numPosts"]).then(async (result) => {
+                if (result.numPosts !== numPosts) {
+                    //@ts-expect-error
+                    await chrome.storage.local.set({ numPosts: numPosts });
+                }
+            });
+        } catch (error) {
+            console.error('Error in the loop:', error);
+            if (chrome.runtime.lastError) {
+                console.error('Chrome runtime error:', chrome.runtime.lastError);
             }
-          });
-        
+        }
+
         await sleep(2000);
     }
 }
-
-main()
